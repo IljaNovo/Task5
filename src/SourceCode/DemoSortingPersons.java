85ba0f836fce24b923a6fab8d3a9d8b1a2637163
Ilya Novozhilov <ilja.keinn@mail.ru>
2015-12-13 20:17:29 +0300

    Create Demonstration Sorting of Persons

---
+++ src/SourceCode/DemoSortingPersons.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class DemoSortingPersons {
+
+    public static void main(String[] args) {
+        Person[] persons = new Person[5];
+
+        persons[0] = new Person("Vasya Pupkin", 1, 1, 1993);
+        persons[1] = new Person("Igor Prokopovich", 1, 4, 1995);
+        persons[2] = new Person("John Doe", 16, 10, 1994);
+        persons[3] = new Person("John Jefferson", 9, 2, 1992);
+        persons[4] = new Person("Stephan Wozniak", 6, 6, 1994);
+
+        Arrays.sort(persons);
+
+        for (Person item : persons) {
+            System.out.println("Name: " + item.getName() +
+                                " Age: " + item.getAge() +
+                                " docNumber: " + item.getDocNumber());
+        }
+    }
+}
