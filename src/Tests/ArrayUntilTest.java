3c6eaa70fb5c17e438694beeb5900719ab45b01d
Ilya Novozhilov <ilja.keinn@mail.ru>
2015-12-13 23:06:19 +0300

    Create tests to ArrayUntil

--- src/Tests/ArrayUntilTest.java
+++ src/Tests/ArrayUntilTest.java
@@ -1,12 +1,47 @@
 import org.junit.Test;
 
-/**
- * Created by Lenovo on 13.12.2015.
- */
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
 public class ArrayUntilTest {
 
     @Test
     public void testSelectValues() throws Exception {
+        Integer[] elems = {1, 2, 3, 4, 5, 6};
+        Integer[] paternAnswer = {2, 4, 6};
 
+        Integer[] answer = ArrayUntil.selectValues(elems, new Predicate<Integer>() {
+
+            @Override
+            public boolean checkCondition(Integer obj) {
+                if ((obj % 2) == 0)
+                    return true;
+                else {
+                    return false;
+                }
+            }
+        });
+        assertTrue(Arrays.equals(paternAnswer, answer));
+    }
+
+    @Test
+    public void testNullArrayInParam() throws Exception {
+        try {
+            ArrayUntil.selectValues(null, new Predicate<Integer>() {
+
+                @Override
+                public boolean checkCondition(Integer obj) {
+                    if ((obj % 2) == 0)
+                        return true;
+                    else {
+                        return false;
+                    }
+                }
+            });
+        }
+        catch (NullArrayExeption e) {
+            assertTrue(true);
+        }
     }
 }
