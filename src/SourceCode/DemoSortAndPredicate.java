aa6d9c647898696d2935f35777c53130290ad094
Ilya Novozhilov <ilja.keinn@mail.ru>
2015-12-13 22:52:16 +0300

    Refactoring Demonstration Sort and Predicate

--- src/SourceCode/DemoSortAndPredicate.java
+++ src/SourceCode/DemoSortAndPredicate.java
@@ -4,24 +4,12 @@ import java.util.Comparator;
 public class DemoSortAndPredicate {
 
     public static void main(String[] args) {
-        // Demonstration Sorting of Persons 
-
+        // Demonstration Sorting of Persons
         Person[] persons = new Person[5];
-
-        persons[0] = new Person("Vasya Pupkin", 1, 1, 1993);
-        persons[1] = new Person("Igor Prokopovich", 1, 4, 1995);
-        persons[2] = new Person("John Doe", 16, 10, 1994);
-        persons[3] = new Person("John Jefferson", 9, 2, 1992);
-        persons[4] = new Person("Stephan Wozniak", 6, 6, 1994);
+        initPersons(persons);
 
         Arrays.sort(persons);
-
-        for (Person item : persons) {
-            System.out.println("Name: " + item.getName() +
-                                " Age: " + item.getAge() +
-                                " docNumber: " + item.getDocNumber());
-        }
-        System.out.println();
+        print(persons);
 
         Arrays.sort(persons, new Comparator<Person>() {
 
@@ -30,15 +18,11 @@ public class DemoSortAndPredicate {
                 return first.getName().compareTo(second.getName());
             }
         });
-        for (Person item : persons) {
-            System.out.println("Name: " + item.getName() +
-                    " Age: " + item.getAge() +
-                    " docNumber: " + item.getDocNumber());
-        }
+        print(persons);
 
         // Demonstration Predicate
         Integer[] elems = {1, 2, 3, 4, 5, 6};
-        
+
         Integer[] searchElems = ArrayUntil.selectValues(elems, new Predicate<Integer>() {
 
             @Override
@@ -50,10 +34,31 @@ public class DemoSortAndPredicate {
                 }
             }
         });
+        print(searchElems);
+    }
+
+    private static void initPersons(Person[] persons) {
+        persons[0] = new Person("Vasya Pupkin", 1, 1, 1993);
+        persons[1] = new Person("Igor Prokopovich", 1, 4, 1995);
+        persons[2] = new Person("John Doe", 16, 10, 1994);
+        persons[3] = new Person("John Jefferson", 9, 2, 1992);
+        persons[4] = new Person("Stephan Wozniak", 6, 6, 1994);
+    }
+
+    private static void print(Integer[] searchElems) {
         int counter = 1;
 
         for (Integer item : searchElems) {
             System.out.format("Item %d: %d%n", counter++, item);
         }
+    }
+
+    private static void print(Person[] persons) {
+        for (Person item : persons) {
+            System.out.println("Name: " + item.getName() +
+                    " Age: " + item.getAge() +
+                    " docNumber: " + item.getDocNumber());
+        }
+        System.out.println();
     }
 }
---
+++ src/Tests/ArrayUntilTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Lenovo on 13.12.2015.
+ */
+public class ArrayUntilTest {
+
+    @Test
+    public void testSelectValues() throws Exception {
+
+    }
+}
